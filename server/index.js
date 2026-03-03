6659dc476ac7787f295f9e3753f0a839626dd139
dnscr7 <durgeshsingh25052003@gmail.com>
2026-03-04 00:07:09 +0530

    install missed package

--- server/index.js
+++ server/index.js
@@ -7,9 +7,7 @@ import "../server/utils/Passport.js";
 
 config({ path: "./.env" });
 
-const DB_raw = process.env.MONGO_URI;
-
-const DB = DB_raw.replace("<db_password>", process.env.DB_PASSWORD);
+const DB = process.env.MONGO_URI;
 
 connect(DB).then(() => {
   console.log("Connected to MongoDB");
