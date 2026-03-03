ee801d09332e92bd942f417ae261258444222d19
dnscr7 <durgeshsingh25052003@gmail.com>
2026-03-04 00:00:41 +0530

    install missed package

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connect(DB).then(() => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
